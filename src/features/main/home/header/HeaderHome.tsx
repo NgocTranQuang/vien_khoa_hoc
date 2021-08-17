547ed03b15853b9e29065c0f2857066e93e2c248
ngoctran <ngoctq110309@gmail.com>
2021-08-17 16:24:19 +0700

    - Finished slibar

--- src/features/main/home/header/HeaderHome.tsx
+++ src/features/main/home/header/HeaderHome.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useAppSelector, useAppDispatch } from '../../../../app/hooks';
 import NavBar from '../../base/components/nav/baseNavBar';
 import { homeHeaderActions, homeHeaderSelectors } from './homeHeaderSlice';
@@ -6,32 +6,44 @@ import './index.css';
 import { Fade } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
 import { translate } from '../../../../i18n/index';
-import { url } from 'inspector';
+
 export interface HeaderHomeProps {
     listImage: string[]
 }
 
+interface ImageThumbnail {
+    img1: string,
+    img2: string
+}
+
+
+
 export default function HeaderHome({ listImage }: HeaderHomeProps) {
 
-    const img1 = "http://gonthemes.info/eschool/wp-content/uploads/2018/11/slider02.jpg"
     const anhNho1 = "http://gonthemes.info/eschool/wp-content/uploads/2018/11/slider02-80x50.jpg"
-
-    const img2 = "http://gonthemes.info/eschool/wp-content/uploads/2018/11/slider01.jpg"
     const anhNho2 = "http://gonthemes.info/eschool/wp-content/uploads/2018/11/slider01-80x50.jpg";
+    const listThumbnail = [anhNho1, anhNho2]
 
-    const currentPosition = useAppSelector(homeHeaderSelectors).currentImage;
-    const headerDispatch = useAppDispatch();
+    const slideRef = useRef<Fade>(null);
 
-    // useEffect(() => {
-    //     var timeInterval = setInterval(() => {
-    //         headerDispatch(homeHeaderActions.nextImage(currentPosition))
-    //     }, 10000)
-    //     return () => {
-    //         clearInterval(timeInterval)
-    //     }
-    // }, [currentPosition])
+    const initState: ImageThumbnail = {
+        img1: listThumbnail[0],
+        img2: listThumbnail[1],
+    }
+    const [thumbnail, setThumbnail] = useState<ImageThumbnail>(initState)
 
-
+    const onChange = (previous: number, current: number) => {
+        setThumbnail({
+            img1: listThumbnail[previous],
+            img2: listThumbnail[previous]
+        })
+    }
+    const preSlide = () => {
+        slideRef.current?.goBack()
+    }
+    const nextSlide = () => {
+        slideRef.current?.goNext()
+    }
     return (
 
         <div className="header-home">
@@ -41,27 +53,25 @@ export default function HeaderHome({ listImage }: HeaderHomeProps) {
                     <div className="hideInPC hideInMobile showInTablet">
                         <NavBar />
                     </div>
-                    <Fade duration={10000} loop={true} transitionDuration={2000} arrows={false}>
+                    <Fade ref={slideRef} duration={10000} loop={true} transitionDuration={2000} arrows={false} onChange={onChange}>
                         <div className="each-fade sizeSlide">
                             <div className="image-container sizeSlide ">
                                 <img className="zoomIn sizeSlide" src={listImage[0]} />
                             </div>
-                            <h2>First Slide</h2>
                         </div>
                         <div className="each-fade sizeSlide">
                             <div className="image-container sizeSlide">
                                 <img className="zoomOut sizeSlide" src={listImage[1]} />
                             </div>
-                            <h2>Second Slide</h2>
                         </div>
                     </Fade>
                 </div>
                 <div className="opaticy0-65 z-index2">
                     <NavBar />
                     <div className="header-hom__content__slide">
-                        <div className="arrow hideInMobile">
+                        <div className="arrow hideInMobile animation-fadein-0-3" onClick={preSlide}>
                             <i className="fas fa-chevron-left center-flex"></i>
-                            <div className="page-imagine" style={{ backgroundImage: `url(${anhNho1})` }}>
+                            <div className="page-imagine animation-fadein-0-3" style={{ backgroundImage: `url(${thumbnail.img1})` }}>
                                 <div className=" matchParent page-imagine page-imagine-left  ">
                                     <div className="page-image__content page-image__content-left matchParent ">
                                         <div className="arrow page-image__conten-arrow">
@@ -75,17 +85,17 @@ export default function HeaderHome({ listImage }: HeaderHomeProps) {
                             </div>
                         </div>
                         <div className="header-content__text">
-                            <div className="header-content__text_title">{translate('How do i get to the university?')}</div>
-                            <div className="header-content-des">
+                            <div className="header-content__text_title animation-left-to-center-0-75">{translate('How do i get to the university?')}</div>
+                            <div className="header-content-des animation-top-to-center-0-75">
                                 <div className="header-content__text_des">{translate('Welcom to Eschool')}</div>
                                 <div className="header-content__text_des" >{translate('Best learning institutions')}</div>
                             </div>
-                            <div className="header-content__text_button background-radius_primary ">{translate('Visit campus').toUpperCase()}</div>
+                            <div className="header-content__text_button background-radius_primary animation-fadein-1-delay">{translate('Visit campus').toUpperCase()}</div>
                         </div>
-                        <div className="arrow hideInMobile">
+                        <div className="arrow hideInMobile animation-fadein-0-3" onClick={nextSlide}>
                             <i className="fas fa-chevron-right center-flex"></i>
 
-                            <div className="page-imagine page-imagine-right" style={{ backgroundImage: `url(${anhNho1})` }}>
+                            <div className="page-imagine page-imagine-right animation-fadein-0-3" style={{ backgroundImage: `url(${thumbnail.img2})` }}>
                                 <div className=" matchParent page-imagine page-imagine-right">
                                     <div className="page-image__content matchParent ">
                                         <div className="arrow page-image__conten-arrow">
