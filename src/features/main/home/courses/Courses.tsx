1cefb0ca92d63a4f19395d5615eb510a90be990a
ngoctran <ngoctq110309@gmail.com>
2021-08-18 18:25:30 +0700

    -Finished making statistic home

---
+++ src/features/main/home/courses/Courses.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { translate } from '../../../../i18n/index';
+import CoursesItem, { CoursesItemProps } from './CoursesItem';
+import './index.css'
+export interface CoursesProps {
+}
+
+export default function Courses(props: CoursesProps) {
+    var listCourse: CoursesItemProps[] = [{
+        name: ""
+    }]
+
+    return (
+        <div className="course">
+            <div className="course__title">
+                {translate('Latest Courses')}
+            </div>
+            <div className="grid wide">
+                <div className="row">
+                    <CoursesItem />
+                    <CoursesItem />
+                    <CoursesItem />
+                    <CoursesItem />
+                </div>
+            </div>
+
+        </div>
+    );
+}
